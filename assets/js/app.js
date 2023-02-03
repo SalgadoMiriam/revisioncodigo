13b177098e9151d00618de009c9377e0c4c70ae2
Salgado Miriam <miriam.slgd@gmail.com>
2023-02-03 15:21:29 -0600

    codigo otros 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const n = document.querySelector('.name');
+const b = document.querySelector('.blog');
+// name y blog son clases cambiar por .
+const one = document.querySelector('.location');
+//no utlizar $ ni numeros como comienzo de caracter de una variable
+// se agrega la clase .location en html
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+// agregar async
+async function displayUser(username) {
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();
+  //devolver respuesta en formato json
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  n.textContent = `${data.name}`;
+  b.textContent = `${data.blog}`;
+  one.textContent = `${data.location}`;
+  //eliminar ' y agregar `
 }
 
 function handleError(err) {
